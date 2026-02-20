7613a440397e51f512178eb76dcbbc5e6ef08d73
derilion <12109650+Derilion@users.noreply.github.com>
2026-02-20 22:08:31 +0100

    frontend lists expenses from backend if connected

--- src/frontend/app/main.py
+++ src/frontend/app/main.py
@@ -5,6 +5,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+import httpx
+
+# Backend configuration
+BACKEND_HOST = "127.0.0.1"
+BACKEND_PORT = 8000
+BACKEND_URL = f"http://{BACKEND_HOST}:{BACKEND_PORT}"
+
 
 app = FastAPI()
 
@@ -13,9 +20,18 @@ templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def landing_page(request: Request):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{BACKEND_URL}/expense/")
+        expenses = response.json()
+
     return templates.TemplateResponse(
         "index.html",
-        {"request": request, "title": "ShareFin", "message": "Welcome to ShareFin"}
+        {
+            "request": request,
+            "title": "ShareFin",
+            "message": "Welcome to ShareFin",
+            "expenses": expenses
+        }
     )
 
 if __name__ == "__main__":
