a3988ba06737f65c3a22c22ec7985e16ca64dc43
derilion <12109650+Derilion@users.noreply.github.com>
2026-02-20 17:25:35 +0100

    added barebones fastapi integration

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python3
 
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "Hello World!"}
+
 def debug():
     main()
 
 def main():
-    print("Hello World!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == '__main__':
-    main()
+    debug()
